ac3498a2a440912938defe12fc1880b12e6c5254
Nischay Venkatram <nischay.v@gmail.com>
2017-06-30 16:57:06 -0700

    Enable movement

--- index.js
+++ index.js
@@ -5,29 +5,50 @@
  *
  * This source requires Phaser 2.6.2
  */
+const game = new Phaser.Game(400, 400, Phaser.AUTO, '', { preload: preload, create: create, update: update, render: render });
 
-const game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update, render: render });
+let human
+let currentAnimation
 
 function preload() {
 
-  this.game.load.atlas('human', './assets/human/human.png', './assets/human/human.json');
+  game.load.atlas('human', './assets/human/human.png', './assets/human/human.json')
 }
 
 function create() {
+  game.physics.startSystem(Phaser.Physics.ARCADE)
 
-  //var human_walk_right = game.add.sprite(300, 300, 'walk_right');
-  //var walk = human_walk_right.animations.add('walk');
-  //human_walk_right.animations.play('walk', 10, true);
-  this.sprite = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'human');
-  this.sprite.animations.add('dying', Phaser.Animation.generateFrameNames('frame', 87, 95), 9, true);
-  this.sprite.animations.play('dying', 10, true);
+  human = game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'human')
+  human.animations.add('walk_right', Phaser.Animation.generateFrameNames('frame', 87, 95), 9, true)
+  human.animations.add('walk_left', Phaser.Animation.generateFrameNames('frame', 69, 77), 9, true)
+  human.animations.add('walk_up', Phaser.Animation.generateFrameNames('frame', 60, 68), 9, true)
+  human.animations.add('walk_down', Phaser.Animation.generateFrameNames('frame', 78, 86), 9, true)
+  //human.animations.play('walk_left', 10, true);
 
 }
 
 function update() {
-
+  if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
+    human.x -= 4;
+    human.animations.play('walk_left', 10, true)
+    currentAnimation = 'walk_left'
+  } else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
+    human.x += 4;
+    human.animations.play('walk_right', 10, true)
+    currentAnimation = 'walk_right'
+  } else if (game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
+    human.y -= 4;
+    human.animations.play('walk_up', 10, true)
+    currentAnimation = 'walk_up'
+  } else if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN)) {
+    human.y += 4;
+    human.animations.play('walk_down', 10, true)
+    currentAnimation = 'walk_down'
+  } else {
+    human.animations.stop(currentAnimation)
+  }
 }
 
 function render() {
-
+  game.debug.spriteInfo(human, 20, 32);
 }
