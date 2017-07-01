ed91b51a37c262a505b4975d55ebcdf833a88917
Nischay Venkatram <nischay.v@gmail.com>
2017-06-30 18:04:09 -0700

    basic world

--- index.js
+++ index.js
@@ -5,18 +5,21 @@
  *
  * This source requires Phaser 2.6.2
  */
-const game = new Phaser.Game(400, 400, Phaser.AUTO, '', { preload: preload, create: create, update: update, render: render });
+const game = new Phaser.Game(650, 350, Phaser.AUTO, '', { preload: preload, create: create, update: update, render: render });
 
 let human
 let currentAnimation
 
 function preload() {
-
+  game.load.image('background', './assets/background/grass_680x382.jpg')
   game.load.atlas('human', './assets/human/human.png', './assets/human/human.json')
 }
 
 function create() {
-  game.physics.startSystem(Phaser.Physics.ARCADE)
+  game.world.setBounds(0, 0, 650, 350)
+  game.add.sprite(0, 0, 'background')
+
+  //game.physics.startSystem(Phaser.Physics.ARCADE)
 
   human = game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'human')
   human.animations.add('walk_right', Phaser.Animation.generateFrameNames('frame', 87, 95), 9, true)
@@ -24,10 +27,11 @@ function create() {
   human.animations.add('walk_up', Phaser.Animation.generateFrameNames('frame', 60, 68), 9, true)
   human.animations.add('walk_down', Phaser.Animation.generateFrameNames('frame', 78, 86), 9, true)
   //human.animations.play('walk_left', 10, true);
-
 }
 
 function update() {
+  game.world.wrap(human, 0, true)
+
   if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
     human.x -= 4;
     human.animations.play('walk_left', 10, true)
@@ -46,9 +50,30 @@ function update() {
     currentAnimation = 'walk_down'
   } else {
     human.animations.stop(currentAnimation)
+    switch (currentAnimation) {
+      case 'walk_left':
+        human.frame = 69
+        break
+      case 'walk_right':
+        human.frame = 87
+        break
+      case 'walk_up':
+        human.frame = 60
+        break
+      case 'walk_down':
+        human.frame = 78
+        break
+      default:
+        human.frame = 78
+    }
   }
 }
 
 function render() {
   game.debug.spriteInfo(human, 20, 32);
 }
+
+//function collectCoin(player, coin) {
+//  coin.kill();
+//}
+
